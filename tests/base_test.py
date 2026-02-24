c684f8bb5d2dd7d84cc93be51f8d05aa666fd9c7
KaushalMore <kaushalmore007@gmail.com>
2026-02-24 14:58:44 +0530

    new logger

--- tests/base_test.py
+++ tests/base_test.py
@@ -6,10 +6,13 @@ from utils.logger import get_logger
 class BaseTest:
 
     @pytest.fixture(autouse=True)
-    def setup(self, driver, config):
+    def setup(self, driver, config, request):
         self.driver = driver
         self.config = config
-        self.logger = get_logger(self.__class__.__name__)
-        self.logger.info("----------------Test Started----------------")
+        # self.logger = get_logger(self.__class__.__name__)
+        self.logger = get_logger(self.__class__.__name__, request)
+        self.logger.info(
+            f"---------------- Test Started {self.__class__.__name__}-{request.node.name} ----------------")
         yield
-        self.logger.info("----------------Test Finished---------------")
+        self.logger.info(
+            f"----------------------------- Test Finished -----------------------------")
