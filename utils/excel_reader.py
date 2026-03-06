9e4d8e160fc0a63a0079518e7570d98529b79e8a
KaushalMore <kaushalmore007@gmail.com>
2026-03-06 09:55:39 +0530

    Updated excel_reader

--- utils/excel_reader.py
+++ utils/excel_reader.py
@@ -3,8 +3,7 @@ import openpyxl
 
 
 def get_test_data():
-    # base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    base_path = os.path.abspath(os.getcwd())  # Jenkins workspace root
+    base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
     file_path = os.path.join(base_path, "test_data", "login_credentials_sauce_demo.xlsx")
 
